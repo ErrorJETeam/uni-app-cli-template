113fecbf2b0a82417bdec1915e4cbbb681be40bb
吴俊胤 <wujunyin@wondersgroup.com>
2020-06-05 08:43:17 +0800

    Update App.vue

--- App.vue
+++ App.vue
@@ -3,7 +3,31 @@ import 'moment/locale/zh-cn'
 
 export default {
 	onLaunch: function() {
-		console.log('App Launch')
+		/* #ifdef MP-WEIXIN */
+		const updateManager = uni.getUpdateManager()
+
+		updateManager.onCheckForUpdate(function(res) {
+			// 请求完新版本信息的回调
+			console.log(res.hasUpdate)
+		})
+
+		updateManager.onUpdateReady(function(res) {
+			uni.showModal({
+				title: '更新提示',
+				content: '新版本已经准备好，是否重启应用？',
+				success(res) {
+					if (res.confirm) {
+						// 新的版本已经下载好，调用 applyUpdate 应用新版本并重启
+						updateManager.applyUpdate()
+					}
+				}
+			})
+		})
+
+		updateManager.onUpdateFailed(function(res) {
+			// 新的版本下载失败
+		})
+		/* #endif */
 	},
 	onShow: function() {
 		console.log('App Show')
@@ -13,7 +37,7 @@ export default {
 	}
 }
 </script>
-	
+
 <style lang="scss">
 /* 每个页面公共css */
 @import 'common/css/uni.css';
