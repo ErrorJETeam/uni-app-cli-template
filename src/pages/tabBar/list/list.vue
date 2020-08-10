0f87dc7b0849d455de28e560752a81c37ac040e8
吴俊胤 <564109763@qq.com>
2020-08-10 16:28:03 +0800

    增加 ESLint
    1 除了项目 ESLint
    2 还要注意配合 vscode
    {
      "eslint.options": {
        "extensions": [".js", ".jsx", ".vue"]
      },
      "files.autoSave": "off", // 文件不自动保存
      "editor.formatOnSave": false, // 不开启
      "editor.codeActionsOnSave": {
        "source.fixAll.eslint": true // 保存自动修复
      }
    }

--- src/pages/tabBar/list/list.vue
+++ src/pages/tabBar/list/list.vue
@@ -1,31 +1,31 @@
 <template>
-	<view>
-		<h1>列表页面</h1>
-		<view class="">列表测试数据长度 {{ apiData.length }}</view>
-	</view>
+  <view>
+    <h1>列表页面</h1>
+    <view class="">列表测试数据长度 {{ apiData.length }}</view>
+  </view>
 </template>
 
 <script>
-import { apiRequestTest } from '@/apis/modules/core.js';
+import { apiRequestTest } from '@/apis/modules/core.js'
 export default {
-	data() {
-		return {
-			apiData: {
-				data: [],
-				length: 0
-			}
-		};
-	},
-	async onLoad() {
-		const {
-			data: { data }
-		} = await apiRequestTest();
-		this.apiData.data = data;
-		this.apiData.length = data.length;
-		console.log('列表页面加载', data);
-	},
-	methods: {}
-};
+  data() {
+    return {
+      apiData: {
+        data: [],
+        length: 0
+      }
+    }
+  },
+  async onLoad() {
+    const {
+      data: { data }
+    } = await apiRequestTest()
+    this.apiData.data = data
+    this.apiData.length = data.length
+    console.log('列表页面加载', data)
+  },
+  methods: {}
+}
 </script>
 
 <style lang="scss"></style>
