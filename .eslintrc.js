976a16fd03a6416ae1a39bcd9ff749444d9e2a5f
吴俊胤 <564109763@qq.com>
2020-08-10 16:40:03 +0800

    增加git hook

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,9 +10,7 @@ module.exports = {
     es6: true
   },
   extends: ['plugin:vue/recommended', 'eslint:recommended'],
-  globals: {
-    uni: false
-  },
+
   // add your custom rules here
   // it is base on https://github.com/vuejs/eslint-config-vue
   rules: {
