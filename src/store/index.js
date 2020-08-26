5100438c199059b4e1e7e66fca38a0cf83ebb361
吴俊胤 <wujunyin@wondersgroup.com>
2020-08-27 06:32:41 +0800

    vuex 根模块抽离

--- src/store/index.js
+++ src/store/index.js
@@ -1,21 +1,13 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import getters from './getters.js'
+import rootModule from './rootModule'
 
 Vue.use(Vuex)
-
-// 根模块
-const rootModule = {
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {},
-  getters // this.$store.getters.xxxx
-}
 
 // vuex 模块集成
 const files = require.context('./modules', false, /\.js$/)
 files.keys().forEach((key, index) => {
+  // 模块名 + 模块值
   const store = files(key).default
   const moduleName = key.replace(/^\.\//, '').replace(/\.js$/, '')
   const modules = rootModule.modules || {}
---
+++ src/store/rootModule.js
@@ -0,0 +1,10 @@
+import getters from './getters.js'
+
+// 根模块
+export default {
+  state: {},
+  mutations: {},
+  actions: {},
+  modules: {},
+  getters // this.$store.getters.xxxx
+}
