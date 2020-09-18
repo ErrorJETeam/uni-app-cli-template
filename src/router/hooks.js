4608729c53fce8bd31fd0ff8a560909704f617ea
吴俊胤 <564109763@qq.com>
2020-09-18 15:23:31 +0800

    [fix] 路由守卫报错

--- src/router/hooks.js
+++ src/router/hooks.js
@@ -8,9 +8,9 @@ export default {
     console.log('请求中断控制')
     // 这里的 this 指向路由实例，所以可以 this.push
     // TODO 监听 tabbar 和 原生返回情况下的页面切换。可以利用 onShow
-    const ajaxToken = store.state.base.requestToken
-    ajaxToken.forEach(fn => fn()) // 页面切换遍历请求中断
-    store.commit(`base/${types.SET_REQUEST_TOKEN}`, 'clear') // 清空 vuex 队列
+    // const ajaxToken = store.state.base.requestToken
+    // ajaxToken.forEach(fn => fn()) // 页面切换遍历请求中断
+    // store.commit(`base/${types.SET_REQUEST_TOKEN}`, 'clear') // 清空 vuex 队列
     next()
   },
 
