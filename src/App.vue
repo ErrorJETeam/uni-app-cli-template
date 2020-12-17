b6330b1ff906c68972b8a87f7cd77046def57e06
吴俊胤 <wujy@helianhealth.com>
2020-12-17 14:39:09 +0800

    [style] format code

--- src/App.vue
+++ src/App.vue
@@ -42,11 +42,14 @@ export default {
 /* 每个页面公共css */
 @import 'common/css/uni.css';
 @import 'common/css/common.scss';
+
 /* colorUi */
 @import 'vendor/colorui/main.css';
 @import 'vendor/colorui/icon.css';
+
 /* 动画库 */
 @import 'common/css/animate.css';
+
 /* uView 样式文件 */
 @import 'uview-ui/index.scss';
 </style>
--- src/common/utils/storage.js
+++ src/common/utils/storage.js
@@ -1,11 +1,9 @@
-import {
-  judgeType
-} from './judege'
+import { judgeType } from './judge'
 
 // 同步设置缓存
 export const setStorageSync = (k, v) => uni.setStorageSync(k, judgeType(v) === 'object' ? JSON.stringify(v) : v)
 // 同步获取缓存
-export const getStorageSync = (k) => {
+export const getStorageSync = k => {
   const storage = uni.getStorageSync(k)
   if (!storage) return undefined
 
